56b40c96712250f200f617b78c9f77471f9d40c9
bujosa <davidbujosa@gmail.com>
2023-05-11 20:01:32 -0400

    feat: add example of message_pattern using macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+macro_rules! message_pattern {
+    ($name:ident, $pattern:expr) => {
+        fn $name(msg: &str) -> bool {
+            regex::Regex::new($pattern).unwrap().is_match(msg)
+        }
+    };
+}
+
+message_pattern!(is_hello, r"^hello .*$");
+message_pattern!(is_goodbye, r"^goodbye .*$");
+
+fn process_message(msg: &str) {
+    if is_hello(msg) {
+        println!("Received a hello message: {}", msg);
+    } else if is_goodbye(msg) {
+        println!("Received a goodbye message: {}", msg);
+    } else {
+        println!("Received an unknown message: {}", msg);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    process_message("hello world");
+    process_message("goodbye cruel world");
+    process_message("foo bar");
 }
